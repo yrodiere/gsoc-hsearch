4e7fcdcc13d262ef035c8dd1a4a2b0167ecae3a9
mincong <mincong.h@gmail.com>
2016-05-30 21:50:44 +0200

    #13 add documentation for reader

--- us-address/src/main/java/io/github/mincongh/batch/BatchItemReader.java
+++ us-address/src/main/java/io/github/mincongh/batch/BatchItemReader.java
@@ -7,10 +7,17 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
- * Read entity IDs from {@code IndexingContext}. Each time, there's an array of 
- * IDs being read. The number depend on array capacity, defined before the job
- * start. The default value is __. These IDs is going to be processed in 
- * {@code BatchItemProcessor}, then be used for Lucene document production.
+ * Read entity IDs from {@code IndexingContext}. Each time, there's one array
+ * being read. The number of IDs inside the array depends on the array capacity.
+ * This value is defined before the job start. Either the default value defined
+ * in the job xml will be applied, or the value overwritten by the user in job
+ * parameters. These IDs will be processed in {@code BatchItemProcessor}, then 
+ * be used for Lucene document production.
+ * <p>
+ * The motivation of using an array of IDs over a single ID is to accelerate
+ * the entity processing. Use a SELECT statement to obtain only one ID is
+ * rather a waste. For more detail about the entity process, please check {@code 
+ * BatchItemProcessor}.
  * 
  * @author Mincong HUANG
  */
@@ -20,12 +27,23 @@ public class BatchItemReader implements ItemReader {
     @Inject
     private IndexingContext indexingContext;
     
+    /**
+     * The checkpointInfo method returns the current checkpoint data for this 
+     * reader. It is called before a chunk checkpoint is committed.
+     * 
+     * @throws Exception thrown for any errors.
+     */
     @Override
     public Serializable checkpointInfo() throws Exception {
         System.out.println("BatchItemReader#checkpointInfo()");
         return null;
     }
 
+    /**
+     * Close operation(s) before the class destruction.
+     * 
+     * @throws Exception thrown for any errors.
+     */
     @Override
     public void close() throws Exception {
         System.out.println("BatchItemReader#close()");
@@ -36,9 +54,9 @@ public class BatchItemReader implements ItemReader {
      * be the first open. If checkpoint exist, then it isn't the first open,
      * provide another open scenario. This mechanism is not used in this demo.
      * 
-     * @param checkpoint The checkpoint info were saved in the batch runtime, 
-     *          previously provided by checkpointInfo().
-     * @throws Exception Any exception occurs during the read.
+     * @param checkpoint The last checkpoint info saved in the batch runtime, 
+     *          previously given by checkpointInfo().
+     * @throws Exception thrown for any errors.
      */
     @Override
     public void open(Serializable checkpoint) throws Exception {
@@ -48,6 +66,8 @@ public class BatchItemReader implements ItemReader {
     /**
      * Read item from the {@code IndexingContext}. Here, item means an array of
      * IDs previously produced by the {@code IdProducerBatchlet}.
+     * 
+     * @throws Exception thrown for any errors.
      */
     @Override
     public Object readItem() throws Exception {
