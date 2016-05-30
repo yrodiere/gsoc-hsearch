c6e8fe1e0ec18d4b279c774a333903e15733dc76
mincong <mincong.h@gmail.com>
2016-05-30 23:48:06 +0200

    #13 add javadoc

--- us-address/src/main/java/io/github/mincongh/batch/BatchItemWriter.java
+++ us-address/src/main/java/io/github/mincongh/batch/BatchItemWriter.java
@@ -6,27 +6,56 @@ import java.util.List;
 import javax.batch.api.chunk.ItemWriter;
 import javax.inject.Named;
 
+/**
+ * Batch item writer writes a list of items into Lucene documents. Here, items 
+ * mean the entities processed by the item processor. These items will be used
+ * to create {@code LuceneWork}. 
+ * 
+ * @author Mincong HUANG
+ */
 @Named
 public class BatchItemWriter implements ItemWriter {
 
+    /**
+     * The checkpointInfo method returns the current checkpoint data for this 
+     * writer. It is called before a chunk checkpoint is committed.
+     * 
+     * @return the checkpoint info
+     * @throws Exception is thrown for any errors.
+     */
     @Override
     public Serializable checkpointInfo() throws Exception {
-        // TODO Auto-generated method stub
         return null;
     }
-
+    
+    /**
+     * The close method marks the end of use of the ItemWriter. The writer
+     * is used to do the cleanup.
+     * 
+     * @throws Exception is thrown for any errors.
+     */
     @Override
     public void close() throws Exception {
-        // TODO Auto-generated method stub
-        
+    
     }
 
+    /**
+     * The open method prepares the writer to write items.
+     * 
+     * @param checkpoint the last checkpoint
+     */
     @Override
-    public void open(Serializable arg0) throws Exception {
-        // TODO Auto-generated method stub
-        
+    public void open(Serializable checkpoint) throws Exception {
+    
     }
 
+    /**
+     * Produce {@code LuceneWork} using the given entities (items).
+     * 
+     * @param items a list of entity-array, {@code List<Clazz[]>}. Each item is 
+     *          an array of entity.
+     * @throw Exception is thrown for any errors.
+     */
     @Override
     public void writeItems(List<Object> items) throws Exception {
         if (items != null) {
