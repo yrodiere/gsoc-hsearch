d6024c63b0e76a3b4a2e91a3a7afd0a64d73fa5a
mincong <mincong.h@gmail.com>
2016-06-13 21:49:46 +0200

    #41 add SQL script for importing yahoo-finance-rht.csv

---
+++ jsr352/src/main/resources/import-yahoo-finance-rht.sql
@@ -0,0 +1,12 @@
+--
+-- Import Yahoo Finance's historical prices in CSV
+-- using company Red Hat, Inc. (RHT)
+--
+-- http://finance.yahoo.com/q/hp?s=RHT&a=07&b=11&c=1999&d=03&e=13&f=2016&g=d
+--
+LOAD DATA LOCAL INFILE '/Users/mincong/Downloads/yahoo-finance-rht.csv'
+  INTO TABLE price
+  FIELDS TERMINATED BY ',' ENCLOSED BY ''
+  IGNORE 1 LINES
+  (`date`, `open`, `high`, `low`, `close`, `volume`, `adj_close`)
+  SET company = 'RHT';
