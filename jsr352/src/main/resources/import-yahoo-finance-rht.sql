a7a3e9358a85298525010ae78f21e2ce043c7f4e
mincong <mincong.h@gmail.com>
2016-06-14 02:24:05 +0200

    #41 add SQL script for importing CSV files

--- jsr352/src/main/resources/import-yahoo-finance-rht.sql
+++ jsr352/src/main/resources/import-yahoo-finance-rht.sql
@@ -5,7 +5,7 @@
 -- http://finance.yahoo.com/q/hp?s=RHT&a=07&b=11&c=1999&d=03&e=13&f=2016&g=d
 --
 LOAD DATA LOCAL INFILE '/Users/mincong/Downloads/yahoo-finance-rht.csv'
-  INTO TABLE price
+  INTO TABLE stock
   FIELDS TERMINATED BY ',' ENCLOSED BY ''
   IGNORE 1 LINES
   (`date`, `open`, `high`, `low`, `close`, `volume`, `adj_close`)
