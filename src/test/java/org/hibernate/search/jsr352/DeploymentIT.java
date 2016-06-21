29e5a249c215e1b319c0bb8d2f460f68041c8a6a
mincong <mincong.h@gmail.com>
2016-06-22 00:11:23 +0200

    #60 add shrink-wrap resolver dependency for war archive creation

--- src/test/java/org/hibernate/search/jsr352/DeploymentIT.java
+++ src/test/java/org/hibernate/search/jsr352/DeploymentIT.java
@@ -1,5 +1,10 @@
 package org.hibernate.search.jsr352;
 
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.BatchStatus;
+import javax.batch.runtime.JobExecution;
+
 import org.hibernate.search.store.IndexShardingStrategy;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -8,6 +13,7 @@ import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -28,18 +34,29 @@ public class DeploymentIT {
     
     @Deployment
     public static WebArchive createDeployment() {
+        /*
         WebArchive war = ShrinkWrap.create(WebArchive.class)
                 // deprecated classes is not shipped in the war, add manually
-                .addClasses(IndexShardingStrategy.class)
+                .addClasses(IndexShardingStrategy.class,
+                        org.hibernate.criterion.Projection.class,
+                        org.hibernate.criterion.Projections.class)
                 .addPackages(true, "org.hibernate.search.jsr352")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"))
                 .addAsResource("META-INF/batch-jobs/mass-index.xml");
         logger.info(war.toString(true));
         return war;
+        */
+        WebArchive war = ShrinkWrap.create(MavenImporter.class)
+                .loadPomFromFile("pom.xml")
+                .importBuildOutput()
+                .as(WebArchive.class);
+        return war;
     }
     
     @Test
     public void testJobStart() {
+        /*
+        // start job
         MassIndexer massIndexer = new MassIndexerImpl()
                 .arrayCapacity(ARRAY_CAPACITY)
                 .fetchSize(FETCH_SIZE)
@@ -50,5 +67,21 @@ public class DeploymentIT {
                 .partitions(PARTITIONS)
                 .purgeAtStart(PURGE_AT_START)
                 .threads(THREADS);
+        long executionId = massIndexer.start();
+        
+        // calculate the performance
+        JobOperator jobOperator = BatchRuntime.getJobOperator();
+        JobExecution execution = jobOperator.getJobExecution(executionId);
+        int i = 0; 
+        while (!execution.getBatchStatus().equals(BatchStatus.COMPLETED) && i < 200) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            i++;
+        }
+        */
     }
 }
