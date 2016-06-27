889e78291241d740c9923b74499b7330fdfd14e2
mincong <mincong.h@gmail.com>
2016-06-27 21:29:30 +0200

    #68 use embedded H2 database for Java SE testing

--- integrationtest/javase/src/test/java/org/hibernate/search/jsr352/se/MassIndexerIT.java
+++ integrationtest/javase/src/test/java/org/hibernate/search/jsr352/se/MassIndexerIT.java
@@ -1,79 +1,85 @@
 package org.hibernate.search.jsr352.se;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.lucene.search.Query;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
 import org.hibernate.search.jsr352.MassIndexer;
 import org.hibernate.search.jsr352.MassIndexerImpl;
+import org.hibernate.search.jsr352.se.test.Company;
+import org.jboss.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MassIndexerIT {
-    
-    private final boolean OPTIMIZE_AFTER_PURGE = true;
-    private final boolean OPTIMIZE_AT_END = true;
-    private final boolean PURGE_AT_START = true;
-    private final int ARRAY_CAPACITY = 500;
-    private final int FETCH_SIZE = 100000;
-    private final int MAX_RESULTS = 1000000;
-    private final int PARTITION_CAPACITY = 500;
-    private final int PARTITIONS = 4;
-    private final int THREADS = 2;
 
-    /*
-     * Test if all params are correctly set
-     */
+    private EntityManagerFactory emf;
+    private EntityManager em;
+    
+    private final Company COMPANY_1 = new Company("Google");
+    private final Company COMPANY_2 = new Company("Red Hat");
+    private final Company COMPANY_3 = new Company("Microsoft");
+    
+    private static final Logger logger = Logger.getLogger(MassIndexerIT.class);
+    
+    @Before
+    public void setupJPA() {
+        
+        emf = Persistence.createEntityManagerFactory("h2");
+        em = emf.createEntityManager();
+        
+        em.getTransaction().begin();
+        em.persist(COMPANY_1);
+        em.persist(COMPANY_2);
+        em.persist(COMPANY_3);
+        em.getTransaction().commit();
+    }
+
     @Test
-    public void testJobParams() {
-        
-        MassIndexer massIndexer = new MassIndexerImpl()
-                .arrayCapacity(ARRAY_CAPACITY)
-                .fetchSize(FETCH_SIZE)
-                .maxResults(MAX_RESULTS)
-                .optimizeAfterPurge(OPTIMIZE_AFTER_PURGE)
-                .optimizeAtEnd(OPTIMIZE_AT_END)
-                .partitionCapacity(PARTITION_CAPACITY)
-                .partitions(PARTITIONS)
-                .purgeAtStart(PURGE_AT_START)
-                .threads(THREADS);
-        
-        assertEquals(ARRAY_CAPACITY, massIndexer.getArrayCapacity());
-        assertEquals(FETCH_SIZE, massIndexer.getFetchSize());
-        assertEquals(MAX_RESULTS, massIndexer.getMaxResults());
-        assertEquals(OPTIMIZE_AFTER_PURGE, massIndexer.isOptimizeAfterPurge());
-        assertEquals(OPTIMIZE_AT_END, massIndexer.isOptimizeAtEnd());
-        assertEquals(PARTITION_CAPACITY, massIndexer.getPartitionCapacity());
-        assertEquals(PARTITIONS, massIndexer.getPartitions());
-        assertEquals(PURGE_AT_START, massIndexer.isPurgeAtStart());
-        assertEquals(THREADS, massIndexer.getThreads());
+    public void testJPA() {
+        Company google = em.find(Company.class, 1L);
+        Company redhat = em.find(Company.class, 2L);
+        Company microsoft = em.find(Company.class, 3L);
+        assertEquals(COMPANY_1.getName(), google.getName());
+        assertEquals(COMPANY_2.getName(), redhat.getName());
+        assertEquals(COMPANY_3.getName(), microsoft.getName());
     }
     
-    /**
-     * Test if the set of root entities is set correctly via toString() method
-     */
     @Test
-    public void testRootEntities_notNull() {
-        
-        Set<Class<?>> rootEntities = new HashSet<>();
-        rootEntities.add(String.class);
-        rootEntities.add(Integer.class);
-        
-        MassIndexer massIndexer = new MassIndexerImpl().rootEntities(rootEntities);
-        Set<Class<?>> _rootEntities = massIndexer.getRootEntities();
-        
-        assertTrue(_rootEntities.contains(String.class));
-        assertTrue(_rootEntities.contains(Integer.class));
+    public void testSearchBeforeIndex() {
+        List<Company> companies = findCompanyByName("google");
+        if (companies.isEmpty()) {
+            logger.info("Company list is empty.");
+        } else {
+            for (Company company: companies) {
+                logger.infof("{id=%d, name=\"%s\"}", company.getId(), company.getName());
+            }
+        }
+        assertEquals(0, companies.size());
     }
     
-    @Test(expected=NullPointerException.class)
-    public void testRootEntities_null() {
-        new MassIndexerImpl().rootEntities(null);
+    @After
+    public void destroyJPA() {
+        em.close();
+        emf.close();
     }
     
-    @Test(expected=NullPointerException.class)
-    public void testRootEntities_empty() {
-        new MassIndexerImpl().rootEntities(new HashSet<Class<?>>());
+    private List<Company> findCompanyByName(String name) {
+        FullTextEntityManager ftem = Search.getFullTextEntityManager(em);
+        Query luceneQuery = ftem.getSearchFactory().buildQueryBuilder()
+                .forEntity(Company.class).get()
+                    .keyword().onField("name").matching(name)
+                .createQuery();
+        @SuppressWarnings("unchecked")
+        List<Company> result = ftem.createFullTextQuery(luceneQuery).getResultList();
+        return result;
     }
 }
