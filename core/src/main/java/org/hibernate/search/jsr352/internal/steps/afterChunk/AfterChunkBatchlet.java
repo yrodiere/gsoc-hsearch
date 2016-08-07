88e89f4504de654d615fb1797751a4e9039e4eb7
mincong <mincong.h@gmail.com>
2016-08-07 15:51:09 +0200

    #119 unwrap session from emf directly

--- core/src/main/java/org/hibernate/search/jsr352/internal/steps/afterChunk/AfterChunkBatchlet.java
+++ core/src/main/java/org/hibernate/search/jsr352/internal/steps/afterChunk/AfterChunkBatchlet.java
@@ -13,11 +13,11 @@ import javax.batch.api.Batchlet;
 import javax.batch.runtime.context.JobContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.search.backend.spi.BatchBackend;
 import org.hibernate.search.hcore.util.impl.ContextHelper;
 import org.hibernate.search.jsr352.internal.JobContextData;
@@ -37,7 +37,7 @@ public class AfterChunkBatchlet implements Batchlet {
 
 	@PersistenceUnit(unitName = "h2")
 	private EntityManagerFactory emf;
-	private EntityManager em;
+	private Session session;
 
 	@Inject
 	@BatchProperty
@@ -54,8 +54,7 @@ public class AfterChunkBatchlet implements Batchlet {
 		if ( this.optimizeAtEnd ) {
 
 			logger.info( "purging index for all entities ..." );
-			em = emf.createEntityManager();
-			Session session = em.unwrap( Session.class );
+			session = emf.unwrap( SessionFactory.class ).openSession();
 			final BatchBackend backend = ContextHelper
 					.getSearchintegrator( session )
 					.makeBatchBackend( null );
@@ -72,7 +71,7 @@ public class AfterChunkBatchlet implements Batchlet {
 	@Override
 	public void stop() throws Exception {
 		try {
-			em.close();
+			session.close();
 		}
 		catch ( Exception e ) {
 			logger.error( e );
