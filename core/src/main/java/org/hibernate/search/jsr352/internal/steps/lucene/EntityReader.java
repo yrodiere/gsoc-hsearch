3cd83d9b5d9c055ee133d66a5af7e9c027a572dc
Mincong HUANG <mincong.h@gmail.com>
2017-01-05 22:22:33 +0100

    #165 delete unused import

--- core/src/main/java/org/hibernate/search/jsr352/internal/steps/lucene/EntityReader.java
+++ core/src/main/java/org/hibernate/search/jsr352/internal/steps/lucene/EntityReader.java
@@ -24,7 +24,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.StatelessSession;
 import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.hibernate.search.jsr352.internal.JobContextData;
 import org.hibernate.search.jsr352.internal.se.JobSEEnvironment;
